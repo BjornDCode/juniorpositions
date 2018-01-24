2189b64956ee9d26f0729e011ab3da996770e772
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-01-24 16:38:28 +0100

    Fix meta info on single job

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -2,10 +2,10 @@
 
 @section('header')
         <title>{{ config('app.name', 'Junior Positions') }}</title>
-        <meta property="og:url" content="{{ url('/') }}" />
+        <meta property="og:url" content="{{ url()->current() }}" />
         <meta property="og:type" content="article" />
-        <meta property="og:title" content="Junior Positions | Find your first job" />
-        <meta property="og:description" content="Easily find available junior positions in development, design, marketing, sales and other industries" />
+        <meta property="og:title" content="{{ $job->title }} at {{ $job->company->name }} | Junior Positions" />
+        <meta property="og:description" content="{{ $job->description }}" />
         <meta property="og:image" content="http://via.placeholder.com/1200x630" />
 @endsection
 
