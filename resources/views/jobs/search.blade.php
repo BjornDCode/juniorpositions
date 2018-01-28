1c7a3dcc5f99b8d5ffff635c70c591fdb49b0206
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-01-28 15:15:03 +0100

    Integrate algolia on search page

--- resources/views/jobs/search.blade.php
+++ resources/views/jobs/search.blade.php
@@ -5,38 +5,77 @@
 @endsection
 
 @section('content')
-    @include('partials.search')
-    <main class="content pushed">
-        <div class="container">
-            <h1>Search</h1>
-            <ul class="jobs">
-                @foreach($jobs as $job)
-                    <li class="job">
-                        <a href="/jobs/{{ $job->id }}">
-                            <h3>{{ $job->title }}</h3>
-                        </a>
-                        <div class="meta info">
-                            <div>
-                                <a href="/company/{{ $job->company->slug }}">{{ $job->company->name }}</a> 
-                                - 
-                                <a href="/location/{{ $job->city->country->slug }}/{{ $job->city->slug }}">
-                                    {{ $job->city->name }}
-                                </a>,
-                                <a href="/location/{{ $job->city->country->slug }}">
-                                    {{ $job->city->country->name }}
-                                </a>
-                            </div>
-                            <div>
-                                @foreach($job->skills as $skill)
-                                    <a href="/skill/{{ $skill->slug }}">{{ $skill->name }}</a>, 
-                                @endforeach
-                            </div>
-                        </div>
-                    </li>
-                @endforeach
+    <ais-index
+    app-id="MR4XR4CQPC"
+    api-key="592376609dd6027eccc750fa5da030ee"
+    index-name="jobs"
+    >
 
-                {{ $jobs->links() }}
-            </ul>
-        </div>
-    </main>
+
+        @include('partials.search')
+        <main class="content pushed">
+            <div class="container">
+                <h1>Search</h1>
+                <ul class="jobs">
+                    <ais-results>
+                        <template slot-scope="{ result }">
+                            <li class="job">
+                                <a :href="'/jobs/' + result.id">
+                                    <h3 v-text="result.title"></h3>
+                                </a>
+                                <div class="meta info">
+                                    <div>
+                                        <a :href="'/company/' + result.company.slug" v-text="result.company.name"></a> 
+                                        - 
+                                        <a 
+                                            :href="'/location/' + result.city.country.slug + '/' +  result.city.slug"
+                                            v-text="result.city.name">
+                                        </a>,
+                                        <a 
+                                            :href="'/location/' + result.city.country.slug"
+                                            v-text="result.city.country.name">
+                                        </a>
+                                    </div>
+                                    <div>
+                                            <a v-for="skill in result.skills"
+                                                :href="'/skill/' + skill.slug" 
+                                                v-text="skill.name + ', '">
+                                            </a> 
+                                    </div>
+                                </div>
+                            </li>
+                        </template>
+                    </ais-results>
+                </ul>
+                {{-- <ul class="jobs">
+                    @foreach($jobs as $job)
+                        <li class="job">
+                            <a href="/jobs/{{ $job->id }}">
+                                <h3>{{ $job->title }}</h3>
+                            </a>
+                            <div class="meta info">
+                                <div>
+                                    <a href="/company/{{ $job->company->slug }}">{{ $job->company->name }}</a> 
+                                    - 
+                                    <a href="/location/{{ $job->city->country->slug }}/{{ $job->city->slug }}">
+                                        {{ $job->city->name }}
+                                    </a>,
+                                    <a href="/location/{{ $job->city->country->slug }}">
+                                        {{ $job->city->country->name }}
+                                    </a>
+                                </div>
+                                <div>
+                                    @foreach($job->skills as $skill)
+                                        <a href="/skill/{{ $skill->slug }}">{{ $skill->name }}</a>, 
+                                    @endforeach
+                                </div>
+                            </div>
+                        </li>
+                    @endforeach
+
+                    {{ $jobs->links() }}
+                </ul> --}}
+            </div>
+        </main>
+    </ais-index>
 @endsection
--- resources/views/partials/search.blade.php
+++ resources/views/partials/search.blade.php
@@ -1,26 +1,6 @@
 <search-bar inline-template>
     <div class="search" :class="{ open: open }" @toggleSearch="toggle">
-        <form action="/search">
-            <div class="flex justify-center flex-wrap">
-                <div class="group">
-                    <label for="location">Location</label>
-                    <input type="text" id="location" name="location" placeholder="City, Country" value="{{ old('location') }}">
-                </div>
-                <div class="group">
-                    <label for="role">Role</label>
-                    <input type="text" id="role" name="role" placeholder="Front End, Back End">
-                </div>
-                <div class="group">
-                    <label for="company">Company</label>
-                    <input type="text" id="company" name="company" placeholder="Google, Facebook">
-                </div>
-                <div class="group">
-                    <label for="skill">Skills</label>
-                    <input type="text" id="skill" name="skill" placeholder="Vue, React">
-                </div>
-            </div>
-            <button type="submit" class="button">Search</button>
-        </form>
+        <ais-search-box></ais-search-box>
         <button class="button search-button hide-mobile" @click="toggle">
             <img src="/images/search.svg" alt="Search">
         </button>
