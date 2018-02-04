61e268242ab0af843eaa56babcf7966f6a939065
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-02-04 15:09:38 +0100

    Auth tests

--- tests/Feature/AdminTest.php
+++ tests/Feature/AdminTest.php
@@ -3,13 +3,27 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
-use Illuminate\Http\Response;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class AdminTest extends TestCase
 {
     use RefreshDatabase;
 
-    
+    /** @test */
+    public function an_authenticated_user_can_access_the_admin_area()
+    {
+        $adminUser = factory('App\User')->create();
+
+        $this->actingAs($adminUser)
+             ->get('/admin')
+             ->assertStatus(200);
+    }
+
+    /** @test */
+    public function a_visitor_cannot_access_the_admin_area()
+    {
+        $this->get('/admin')
+             ->assertRedirect('/admin/login');
+    }
 
 }
